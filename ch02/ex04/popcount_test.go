1c314b409aecfe33f8d74e1150520eb0b6cf5ab9
teranaka <in.the.temple@gmail.com>
2017-04-27 01:42:40 +0900

    no message

---
+++ ch02/ex04/popcount_test.go
@@ -0,0 +1,54 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+package popcount_test
+
+import (
+	"testing"
+)
+
+// pc[i] is the population count of i.
+var pc [256]byte
+
+func init() {
+	for i := range pc {
+		pc[i] = pc[i/2] + byte(i&1)
+	}
+}
+
+func PopCount(x uint64) int {
+	return int(pc[byte(x>>(0*8))] +
+		pc[byte(x>>(1*8))] +
+		pc[byte(x>>(2*8))] +
+		pc[byte(x>>(3*8))] +
+		pc[byte(x>>(4*8))] +
+		pc[byte(x>>(5*8))] +
+		pc[byte(x>>(6*8))] +
+		pc[byte(x>>(7*8))])
+}
+
+// PopCount returns the population count (number of set bits) of x.
+func PopCount2(x uint64) int {
+	var val = 0
+	var i uint = 0
+	for i=0; i<64; i++{
+		if (byte(x>>i)&1) > 0 {
+			val++
+		}
+	}
+	return val
+}
+
+// -- Benchmarks --
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCount2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount2(0x1234567890ABCDEF)
+	}
+}
